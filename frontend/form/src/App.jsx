c99e281f21d3e3ba2d1dead5988d79629fcd10a2
Vithurshan2002 <sangervithu2002@gmail.com>
2025-05-14 00:23:40 +0530

    added

--- frontend/form/src/App.jsx
+++ frontend/form/src/App.jsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import{useForm}from 'react-hook-form';
+import { FaHandHoldingHeart } from "react-icons/fa";
 function App() {
-const{register,handleSubmit,reset,formState:{errors,isDirty,isValid}}=useForm();
-  console.log(isDirty);
-   console.log(isValid);
+const{register,handleSubmit,reset,formState:{errors,isDirty,isSubmitted,isSubmitSuccessful,isSubmitting}}=useForm();
+  console.log(isSubmitting,isSubmitSuccessful,isSubmitted);
+  
+  const[formshow,setformshow]=useState(true);
+
+  useEffect(()=>{
+    if(isSubmitSuccessful){
+         setformshow(false) 
+    }
+  //form successa summit akidendal forma hide akidu mesgea kaduvam
+    },[isSubmitSuccessful])
+
   return (
     <div className=" flex bg-blue-200 h-screen justify-center items-center">
-      <form onSubmit={handleSubmit((data)=>{
-            console.log(data);
+    {
+      formshow?<form  onSubmit={handleSubmit((data)=>{
+          console.log(data);
             reset();
       })}>
 <div className="bg-blue-400 p-4 text-xl font-extrabold text-blue-800 rounded-4xl">
@@ -20,13 +33,17 @@ const{register,handleSubmit,reset,formState:{errors,isDirty,isValid}}=useForm();
      <div  className=" flex justify-around border-1 p-1.5 rounded-2xl bg-white mt-2 active:outline-4  outline-blue-300"> Password:- <input {...register('password',{required:"* PassWord Field is Required.",maxLength:{value:10,message:'Maximun Length is 10 Characters'}})} type="text"placeholder='Enter Your Email Password'  className="border-2 border-blue-900 outline-0 rounded-md ps-1  w-full ms-4 focus:outline-2 outline-blue-400 text-lg italic text-black" /></div>
      {errors?.password&&<p className='text-red-600 text-xs font-medium ps-4 mb-2'>{errors.password.message}</p>}
      <div  className=" flex justify-around border-1 p-1.5 rounded-2xl bg-white mt-2 active:outline-4  outline-blue-300">  ContactNumber:- <input {...register('phone',{required:"* ContactNumber Field is Required.",maxLength:{value:10,message:"Contact Number Doesnot Exceed 10 Characters."},minLength:{value:10,message:"Contact Number Doesnot Conatain Lessthan 10 Characters"}})} type="text" placeholder='Enter Your Contactumber' className="border-2 border-blue-900 outline-0 rounded-md ps-1  w-full ms-4 focus:outline-2 outline-blue-400 text-lg italic text-black" /></div>
-     {errors?.phone&&<p className='text-red-600 text-xs font-medium ps-4 mb-2'>{errors.phone.message}</p>}
+     {errors?.phone&&<p className='text-red-600 text-xs font-medium ps-4 mb-2'>{errors.phone.message}</p>}  
        <div  className="flex justify-evenly pt-6 text-white "> 
-         <div> <input type="submit" value='Submit' disabled={!isValid || !isDirty} className="border-2 bg-green-500 rounded-3xl p-2 hover:cursor-pointer hover:text-black hover:bg-green-300"/></div>
-          <div><input type="reset" value='Reset' className="border-2 bg-red-500 rounded-3xl p-2 hover:cursor-pointer hover:text-black hover:bg-red-300"/></div>
+         <div> <input type="submit" value='Submit' disabled={ !isDirty} className="border-2 bg-green-500 rounded-3xl p-2 hover:cursor-pointer hover:text-black hover:bg-green-300"/></div>
+          <div><input type="button" onClick={()=>{reset()}} value='Reset' className="border-2 bg-red-500 rounded-3xl p-2 hover:cursor-pointer hover:text-black hover:bg-red-300"/></div>
       </div>
   </div>
-    </form>
+    </form>: <h1 className='bg-gray-500 p-20 font-extrabold border-2 rounded-4xl text-center shadow-2xl shadow-black-400  text-3xl '><div className='flex justify-center '>{<FaHandHoldingHeart  className='text-red-400 animate-[bounce_3S_ease-in-out_infinite]   w-[300px] h-[200px] pb-4' />}</div><p className='text-white'>Form is SuccessFully Submitted..</p><button onClick={()=>{setformshow(true)}} className='text-xs underline text-black pt-2 hover:cursor-pointer'>Do You Want to another response</button></h1>
+
+    }  
+
+    
     </div>
   )
 }
